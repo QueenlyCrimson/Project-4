d3780024d1172f28b18eb61b8f7b13b639bce6eb
QueenlyCrimson <ccinemainn@gmail.com>
2023-03-30 18:42:28 -0500

    message model made and chatroom made

--- models/chatroom.js
+++ models/chatroom.js
@@ -7,7 +7,14 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {}
+    static associate(models) {
+      Chatroom.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
+      Chatroom.hasMany(models.Message, {
+        foreignKey: "messageId",
+      });
+    }
   }
   Chatroom.init(
     {
