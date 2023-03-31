c453aafc7339fb67003b9dc00c3bcfcd850f3ea1
QueenlyCrimson <ccinemainn@gmail.com>
2023-03-30 19:26:11 -0500

    initial migration testing

--- migrations/20230331001328-create-chattingroom.js
+++ migrations/20230331001328-create-chattingroom.js
@@ -10,35 +10,35 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       userId1: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
       userId2: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
       userId3: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: true,
       },
       userId4: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: true,
       },
       userId5: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: true,
       },
       userId6: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: true,
       },
       userId7: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: true,
       },
       userId8: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: true,
       },
       createdAt: {
