a3d22c2370f92a876b5dcaf0aa1d09c33b750aa8
QueenlyCrimson <ccinemainn@gmail.com>
2023-04-03 13:54:10 -0500

    controllers for chatroom and chattingroom have been updated

--- controllers/ChatroomController.js
+++ controllers/ChatroomController.js
@@ -1,1 +1,43 @@
 const { Chatroom } = require("../models");
+
+const GetChatrooms = async (req, res) => {
+  try {
+    const data = await Chatroom.findAll();
+    res.send(data);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const CreateChatroom = async (req, res) => {
+  try {
+    let chatroomBody = {
+      ...req.body,
+    };
+    let newChatroom = await Chatroom.create(chatroomBody);
+    res.send(newChatroom);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const UpdateChatroom = async (req, res) => {
+  try {
+    const id = parseInt(req.params.chatId);
+    const updatedChatroom = req.body;
+    let updateChatroom = await Chatroom.update(
+      {
+        name: updatedChatroom.name,
+        image: updatedChatroom.image,
+      },
+      { where: { id: id } }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  GetChatrooms,
+  CreateChatroom,
+};
