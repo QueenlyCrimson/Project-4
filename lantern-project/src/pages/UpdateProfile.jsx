b45c97d9e4c9838f4542004f6aae6886055de09d
QueenlyCrimson <ccinemainn@gmail.com>
2023-04-04 01:17:16 -0500

    UpdateProfile functional

--- lantern-project/src/pages/UpdateProfile.jsx
+++ lantern-project/src/pages/UpdateProfile.jsx
@@ -36,8 +36,8 @@ const UpdateProfile = () => {
     }
     await UpdateUser(payload)
     setFormValues(initialState)
-    navigate('/feed')
-    alert("Profile Updated!!")
+    navigate('/')
+    alert("Profile Updated!")
   }
 
   const handleChange = (e) => {
@@ -47,40 +47,42 @@ const UpdateProfile = () => {
   return (
     <div>
     <form onSubmit={handleSubmit}>
-              <div className="overflow-hidden shadow sm:rounded-md">
-                <div className="bg-white px-4 py-5 sm:p-6">
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                        Name
-                      </label>
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        value={formValues.name}
-                        onChange={handleChange}
-                        autoComplete="given-name"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+              <div className="">
+                <div className="">
+                  <div className="">
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    <div className="">
+                      <label htmlFor="first-name" className="">
                         Username
                       </label>
                       <input
                         type="text"
-                        name="userName"
-                        id="userName"
-                        value={formValues.userName}
+                        name="username"
+                        id="username"
+                        value={formValues.username}
                         onChange={handleChange}
-                        autoComplete="family-name"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                        autoComplete="username"
+                        className=""
                       />
                     </div>
-                    <div className="col-span-6 sm:col-span-4">
-                      <label htmlFor="email-address" className="block text-sm font-medium leading-6 text-gray-900">
+
+                    <div className="">
+                      <label htmlFor="last-name" className="">
+                        Profile Picture
+                      </label>
+                      <input
+                        type="text"
+                        name="profilePic"
+                        id="profilePic"
+                        value={formValues.profilePic}
+                        onChange={handleChange}
+                        autoComplete="family-name"
+                        className=""
+                      />
+                    </div>
+
+                    <div className="">
+                      <label htmlFor="email-address" className="">
                         Email address
                       </label>
                       <input
@@ -90,15 +92,16 @@ const UpdateProfile = () => {
                         value={formValues.email}
                         onChange={handleChange}
                         autoComplete="email"
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                        className=""
                       />
                     </div>
+
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                <div className="">
                   <button
                     type="submit"
-                    className="inline-flex justify-center rounded-md bg-[#e57626] py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-[#e99253] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    className=""
                   >
                     Update Profile
                   </button>
