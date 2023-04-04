cbaeba9635321e9a3c6c12f7fcb7f255d81b7e73
QueenlyCrimson <ccinemainn@gmail.com>
2023-04-04 02:22:17 -0500

    handleDelete Route made

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -8,5 +8,6 @@ Router.get("/get_user/by_email/:email", controller.GetUserByEmail);
 Router.post("/create_user", controller.CreateUser);
 Router.get("/get_friends/:user_id", controller.GetUserFriends);
 Router.put("/update_user/by_id/:user_id", controller.UpdateUser);
+Router.delete("/delete_user/:user_id", controller.DeleteUser);
 
 module.exports = Router;
